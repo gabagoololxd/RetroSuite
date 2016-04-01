8d001dfd7aad185e43792feda4ebca082d4d2204
Kyle Corbelli <kyle.corbelli@gmail.com>
2016-04-01 14:24:10 -0700

    Further fill out IPFS implementation

--- chromeApp/frontend/emulator/games/gamesList.js
+++ chromeApp/frontend/emulator/games/gamesList.js
@@ -1,7 +1,7 @@
 var gamesList = [
   {
     title: 'Crono Trigger',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/Qmdr9mDKwUiUgEp8aFPktFB8wDZtnY9RvJWcf4ncinjpWh/Chrono%20Trigger%20%28USA%29.zip',
     console: {
       id: 2,
       name: 'SNES'
@@ -9,7 +9,7 @@ var gamesList = [
   },
   {
     title: 'Donkey Kong Country',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/Qmdr9mDKwUiUgEp8aFPktFB8wDZtnY9RvJWcf4ncinjpWh/Donkey%20Kong%20Country%20%28USA%29.zip',
     console: {
       id: 2,
       name: 'SNES'
@@ -17,7 +17,7 @@ var gamesList = [
   },
   {
     title: 'EarthBound',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/Qmdr9mDKwUiUgEp8aFPktFB8wDZtnY9RvJWcf4ncinjpWh/EarthBound%20%28USA%29.zip',
     console: {
       id: 2,
       name: 'SNES'
@@ -25,7 +25,7 @@ var gamesList = [
   },
   {
     title: 'Kirby Super Star',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/Qmdr9mDKwUiUgEp8aFPktFB8wDZtnY9RvJWcf4ncinjpWh/Kirby%20Super%20Star%20%28USA%29.zip',
     console: {
       id: 2,
       name: 'SNES'
@@ -33,7 +33,7 @@ var gamesList = [
   },
   {
     title: 'Legend of Zelda - A Link to the Past',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/Qmdr9mDKwUiUgEp8aFPktFB8wDZtnY9RvJWcf4ncinjpWh/Legend%20of%20Zelda,%20The%20-%20A%20Link%20to%20the%20Past%20%28USA%29.zip',
     console: {
       id: 2,
       name: 'SNES'
@@ -41,7 +41,7 @@ var gamesList = [
   },
   {
     title: 'Megaman X',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/Qmdr9mDKwUiUgEp8aFPktFB8wDZtnY9RvJWcf4ncinjpWh/Megaman%20X%20%28USA%29.zip',
     console: {
       id: 2,
       name: 'SNES'
@@ -49,7 +49,7 @@ var gamesList = [
   },
   {
     title: 'Super Mario Kart',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/Qmdr9mDKwUiUgEp8aFPktFB8wDZtnY9RvJWcf4ncinjpWh/Super%20Mario%20Kart%20%28USA%29%20%281%29.zip',
     console: {
       id: 2,
       name: 'SNES'
@@ -57,7 +57,7 @@ var gamesList = [
   },
   {
     title: 'Super Mario RPG - Legend of the Seven Stars',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/Qmdr9mDKwUiUgEp8aFPktFB8wDZtnY9RvJWcf4ncinjpWh/Super%20Mario%20RPG%20-%20Legend%20of%20the%20Seven%20Stars%20%28USA%29%20%281%29.zip',
     console: {
       id: 2,
       name: 'SNES'
@@ -65,7 +65,7 @@ var gamesList = [
   },
   {
     title: 'Super Mario World',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/Qmdr9mDKwUiUgEp8aFPktFB8wDZtnY9RvJWcf4ncinjpWh/Super%20Mario%20World%20%28USA%29.zip',
     console: {
       id: 2,
       name: 'SNES'
@@ -73,7 +73,7 @@ var gamesList = [
   },
   {
     title: 'Supre Mario World 2 - Yoshi\'s Island',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/Qmdr9mDKwUiUgEp8aFPktFB8wDZtnY9RvJWcf4ncinjpWh/Super%20Mario%20World%202%20-%20Yoshi%27s%20Island%20%28USA%29.zip',
     console: {
       id: 2,
       name: 'SNES'
@@ -81,7 +81,7 @@ var gamesList = [
   },
   {
     title: 'Kirby\'s Dream Land',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/QmU5NpmRnnESeYX4S4V29VHSRUvetQ9qM9a8khqYgXgJyi/Kirby%27s%20Dream%20Land%20%28USA,%20Europe%29.zip',
     console: {
       id: 3,
       name: 'GB'
@@ -89,7 +89,7 @@ var gamesList = [
   },
   {
     title: 'Legend of Zelda Link\'s Awakening',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/QmU5NpmRnnESeYX4S4V29VHSRUvetQ9qM9a8khqYgXgJyi/Legend%20of%20Zelda,%20The%20-%20Link%27s%20Awakening%20%28USA,%20Europe%29.zip',
     console: {
       id: 3,
       name: 'GB'
@@ -97,7 +97,7 @@ var gamesList = [
   },
   {
     title: 'Metroid II',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/QmU5NpmRnnESeYX4S4V29VHSRUvetQ9qM9a8khqYgXgJyi/Metroid%20II%20-%20Return%20of%20Samus%20%28World%29.zip',
     console: {
       id: 3,
       name: 'GB'
@@ -105,7 +105,7 @@ var gamesList = [
   },
   {
     title: 'Pokemon - Blue Version',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/QmU5NpmRnnESeYX4S4V29VHSRUvetQ9qM9a8khqYgXgJyi/Pokemon%20-%20Blue%20Version%20%28USA,%20Europe%29.zip',
     console: {
       id: 3,
       name: 'GB'
@@ -113,7 +113,7 @@ var gamesList = [
   },
   {
     title: 'Pokemon - Red Version',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/QmU5NpmRnnESeYX4S4V29VHSRUvetQ9qM9a8khqYgXgJyi/Pokemon%20-%20Red%20Version%20%28USA,%20Europe%29.zip',
     console: {
       id: 3,
       name: 'GB'
@@ -121,7 +121,7 @@ var gamesList = [
   },
   {
     title: 'Super Mario Land (World)',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/QmU5NpmRnnESeYX4S4V29VHSRUvetQ9qM9a8khqYgXgJyi/Super%20Mario%20Land%20%28World%29.zip',
     console: {
       id: 3,
       name: 'GB'
@@ -129,7 +129,7 @@ var gamesList = [
   },
   {
     title: 'Super Mario Land 2 - 6 Golden Coins',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/QmU5NpmRnnESeYX4S4V29VHSRUvetQ9qM9a8khqYgXgJyi/Super%20Mario%20Land%202%20-%206%20Golden%20Coins%20%28USA,%20Europe%29.zip',
     console: {
       id: 3,
       name: 'GB'
@@ -137,7 +137,7 @@ var gamesList = [
   },
   {
     title: 'Tetris (World)',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/QmU5NpmRnnESeYX4S4V29VHSRUvetQ9qM9a8khqYgXgJyi/Tetris%20%28World%29.zip',
     console: {
       id: 3,
       name: 'GB'
@@ -145,7 +145,7 @@ var gamesList = [
   },
   {
     title: 'Wario Land - Super Mario Land 3 (World)',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/QmU5NpmRnnESeYX4S4V29VHSRUvetQ9qM9a8khqYgXgJyi/Wario%20Land%20-%20Super%20Mario%20Land%203%20%28World%29.zip',
     console: {
       id: 3,
       name: 'GB'
@@ -153,7 +153,7 @@ var gamesList = [
   },
   {
     title: 'Golden Sun',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/QmdN73KMiDTxXEq5MwXP6SvvCTquhR8gCXrvKRdyzcnRo9/0171%20-%20Golden%20Sun%20%28U%29%28Mode7%29.zip',
     console: {
       id: 4,
       name: 'GBA'
@@ -161,7 +161,7 @@ var gamesList = [
   },
   {
     title: 'Metroid - Fusion',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/QmdN73KMiDTxXEq5MwXP6SvvCTquhR8gCXrvKRdyzcnRo9/0690%20-%20Metroid%20-%20Fusion%20%28U%29%28GBANow%29.zip',
     console: {
       id: 4,
       name: 'GBA'
@@ -169,7 +169,7 @@ var gamesList = [
   },
   {
     title: 'Pokemon - Ruby',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/QmdN73KMiDTxXEq5MwXP6SvvCTquhR8gCXrvKRdyzcnRo9/0907%20-%20Pokemon%20Ruby%20%28U%29%28Mugs%29.zip',
     console: {
       id: 4,
       name: 'GBA'
@@ -177,7 +177,7 @@ var gamesList = [
   },
   {
     title: 'Super Mario Advance 4 - Super Mario Bros 3',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/QmdN73KMiDTxXEq5MwXP6SvvCTquhR8gCXrvKRdyzcnRo9/1212%20-%20Super%20Mario%20Advance%204%20-%20Super%20Mario%20Bros%203%20%28U%29%28Independent%29.zip',
     console: {
       id: 4,
       name: 'GBA'
@@ -185,7 +185,7 @@ var gamesList = [
   },
   {
     title: 'Pokemon - Fire Red',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/QmdN73KMiDTxXEq5MwXP6SvvCTquhR8gCXrvKRdyzcnRo9/1636%20-%20Pokemon%20Fire%20Red%20%28U%29%28Squirrels%29.zip',
     console: {
       id: 4,
       name: 'GBA'
@@ -193,7 +193,7 @@ var gamesList = [
   },
   {
     title: 'Pokemon - Leaf Green',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/QmdN73KMiDTxXEq5MwXP6SvvCTquhR8gCXrvKRdyzcnRo9/1637%20-%20Pokemon%20Leaf%20Green%20%28U%29%28Independent%29.zip',
     console: {
       id: 4,
       name: 'GBA'
@@ -201,7 +201,7 @@ var gamesList = [
   },
   {
     title: 'Legend of Zelda, The Minish Cap',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/QmdN73KMiDTxXEq5MwXP6SvvCTquhR8gCXrvKRdyzcnRo9/1865%20-%20The%20Legend%20of%20Zelda%20-%20The%20Minish%20Cap%20%28U%29%28DCS%29.zip',
     console: {
       id: 4,
       name: 'GBA'
@@ -209,7 +209,7 @@ var gamesList = [
   },
   {
     title: 'Pokemon - Emerald',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/QmdN73KMiDTxXEq5MwXP6SvvCTquhR8gCXrvKRdyzcnRo9/1986%20-%20Pokemon%20Emerald%20%28U%29%28TrashMan%29.zip',
     console: {
       id: 4,
       name: 'GBA'
@@ -217,7 +217,7 @@ var gamesList = [
   },
   {
     title: 'Final Fantasy VI Advance',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/QmdN73KMiDTxXEq5MwXP6SvvCTquhR8gCXrvKRdyzcnRo9/2689%20-%20Final%20Fantasy%20VI%20Advance%20%28U%29%28Xenophobia%29.zip',
     console: {
       id: 4,
       name: 'GBA'
@@ -225,7 +225,7 @@ var gamesList = [
   },
   {
     title: 'Castlevania',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/QmdpAWfLFN6bcspfHRY5Vdxt7x3WMQdCo7eoZwnX5N9z33/Castlevania%20%28USA%29.zip',
     console: {
       id: 1,
       name: 'NES'
@@ -233,7 +233,7 @@ var gamesList = [
   },
   {
     title: 'Contra',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/QmdpAWfLFN6bcspfHRY5Vdxt7x3WMQdCo7eoZwnX5N9z33/Contra%20%28USA%29.zip',
     console: {
       id: 1,
       name: 'NES'
@@ -241,7 +241,7 @@ var gamesList = [
   },
   {
     title: 'Legend of Zelda',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/QmdpAWfLFN6bcspfHRY5Vdxt7x3WMQdCo7eoZwnX5N9z33/Legend%20of%20Zelda,%20The%20%28USA%29.zip',
     console: {
       id: 1,
       name: 'NES'
@@ -249,7 +249,7 @@ var gamesList = [
   },
   {
     title: 'Mega Man',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/QmdpAWfLFN6bcspfHRY5Vdxt7x3WMQdCo7eoZwnX5N9z33/Mega%20Man%20%28USA%29.zip',
     console: {
       id: 1,
       name: 'NES'
@@ -257,7 +257,7 @@ var gamesList = [
   },
   {
     title: 'Mega Man 2',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/QmdpAWfLFN6bcspfHRY5Vdxt7x3WMQdCo7eoZwnX5N9z33/Mega%20Man%202%20%28USA%29.zip',
     console: {
       id: 1,
       name: 'NES'
@@ -265,7 +265,7 @@ var gamesList = [
   },
   {
     title: 'Mike Tyson\'s Munch-Out!!',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/QmdpAWfLFN6bcspfHRY5Vdxt7x3WMQdCo7eoZwnX5N9z33/Mike%20Tyson%27s%20Punch-Out!!%20%28USA%29.zip',
     console: {
       id: 1,
       name: 'NES'
@@ -273,7 +273,7 @@ var gamesList = [
   },
   {
     title: 'Super Mario Bros',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/QmdpAWfLFN6bcspfHRY5Vdxt7x3WMQdCo7eoZwnX5N9z33/Super%20Mario%20Bros.%20%28Japan,%20USA%29%20%281%29.zip',
     console: {
       id: 1,
       name: 'NES'
@@ -281,7 +281,7 @@ var gamesList = [
   },
   {
     title: 'Super Mario Bros 2',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/QmdpAWfLFN6bcspfHRY5Vdxt7x3WMQdCo7eoZwnX5N9z33/Super%20Mario%20Bros.%202%20%28USA%29.zip',
     console: {
       id: 1,
       name: 'NES'
@@ -289,7 +289,7 @@ var gamesList = [
   },
   {
     title: 'Super Mario Bros 3',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/QmdpAWfLFN6bcspfHRY5Vdxt7x3WMQdCo7eoZwnX5N9z33/Super%20Mario%20Bros.%203%20%28USA%29.zip',
     console: {
       id: 1,
       name: 'NES'
@@ -297,7 +297,7 @@ var gamesList = [
   },
   {
     title: 'Zelda II - The Adventure of Link',
-    link: '',
+    link: 'http://gateway.ipfs.io/ipfs/QmdpAWfLFN6bcspfHRY5Vdxt7x3WMQdCo7eoZwnX5N9z33/Zelda%20II%20-%20The%20Adventure%20of%20Link%20%28USA%29.zip',
     console: {
       id: 1,
       name: 'NES'
