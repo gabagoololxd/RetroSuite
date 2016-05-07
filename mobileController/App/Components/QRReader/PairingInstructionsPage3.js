c8130ff102956726164a831bb19cda10c36dfc6c
franklinshieh <franklinshieh@gmail.com>
2016-05-06 20:32:21 -0700

    stylize page 3

--- mobileController/App/Components/QRReader/PairingInstructionsPage3.js
+++ mobileController/App/Components/QRReader/PairingInstructionsPage3.js
@@ -31,9 +31,40 @@ class PairingInstructionsPage3 extends React.Component {
     return (
       <View style={styles.instructionsPage}>
         
-        <View style={styles.iconContainer}>
-          <IconIon name="ios-game-controller-a-outline" size={ windowWidth * (315/375)} allowFontScaling={false} color="rgba(132,99,135,.4)" style={styles.flashIcon} />
-        </View>
+          
+          <View style={styles.topButtonContainer}>
+            <View style={styles.gamesListIcon}>
+              <IconIon name="ios-list-outline" size={ windowWidth * (150/375)} allowFontScaling={false} color="rgba(21,21,20,.5)" style={styles.flashIcon} />
+            </View>
+
+            <View style={styles.arrowIcon}>
+              <IconIon name="arrow-down-c" size={ windowWidth * (75/375)} allowFontScaling={false} color="rgba(21,21,20,.5)" style={styles.flashIcon} />
+            </View>
+          </View>
+
+          <View style={styles.dragBox}/>
+
+
+          <View style={styles.buttomButtonContainer}>
+
+            <View style={styles.circleIcons}>
+              <View style={styles.leftCircleIcon}>
+                <IconIon name="ios-circle-outline" size={ windowWidth * (40/375)} allowFontScaling={false} color="rgba(21,21,20,.5)" style={styles.flashIcon} />
+              </View>
+              <View style={styles.rightCircleIcon}>
+                <IconIon name="ios-circle-filled" size={ windowWidth * (40/375)} allowFontScaling={false} color="rgba(132,99,135,.4)" style={styles.flashIcon} />
+              </View>
+            </View>
+
+            <View style={styles.controllerCord}/>
+            <View style={styles.controllerIcon}>
+              <IconIon name="ios-game-controller-a-outline" size={ windowWidth * (110/375)} allowFontScaling={false} color="rgba(21,21,20,.5)" style={styles.flashIcon} />
+            </View>
+
+          </View>
+
+
+
           
         <View style={styles.text}>
           <Text style={styles.header}>{"3. Choose a game"}</Text>
@@ -58,13 +89,66 @@ const styles = StyleSheet.create({
     borderRadius: windowWidth * (10/375),
     padding: windowWidth * (20/414),
   },
-  iconContainer: {
-    flex: 8,
-    justifyContent: 'center',
-    alignItems: 'center',
+  topButtonContainer: {
+    borderBottomColor: 'rgba(21,21,20,.5)',
+    borderBottomWidth: 2,
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    flexDirection: 'row',
+    flex: 1,
+    paddingBottom: windowWidth * (30/375),
+  },
+  gamesListIcon: {
+    marginLeft: windowWidth * (20/375),
+    marginRight: windowWidth * (10/375),
+  },
+  arrowIcon: {
+    marginTop: windowWidth * (10/375),
+    marginLeft: windowWidth * (10/375),
+    marginRight: windowWidth * (45/375),
+  },
+  dragBox: {
+    width: windowWidth * (90/375),
+    height: windowWidth * (45/375),
+    position: 'absolute',
+    top: windowWidth * (103/375),
+    right: windowWidth * (40/375),
+    borderWidth: 4,
+    borderColor: 'rgba(21,21,20,.5)',
+    borderStyle: 'dashed'
+  },
+  buttomButtonContainer: {
+    flex: 1,
+  },
+  circleIcons: {
+    paddingTop: windowWidth * (30/375),
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    marginBottom: windowWidth * (-40/375)
+  },
+  leftCircleIcon: {
+    marginLeft: windowWidth * (50/375),
+    marginRight: windowWidth * (50/375),
+  },
+  rightCircleIcon: {
+    marginLeft: windowWidth * (50/375),
+    marginRight: windowWidth * (50/375),
+  },
+  controllerCord: {
+    position: 'absolute',
+    top: windowWidth * (10/375),
+    left: windowWidth * (135/375),
+    width: windowWidth * (5/375),
+    height:  windowWidth * (55/375),
+    backgroundColor: 'rgba(21,21,20,.5)'
+  },
+  controllerIcon: {
+    alignSelf: 'center',
+    marginBottom: windowWidth * (-40/375)
   },
   text: {
-    flex: 6,
+    flex: 1,
     marginTop:  windowWidth * (10/414),
     marginHorizontal: windowWidth * (19/414),
   },
